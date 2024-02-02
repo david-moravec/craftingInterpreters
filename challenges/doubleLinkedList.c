02aae310ad85232f306f06eb16bde08bbec38973
David Moravec <moravec@numsolution.cz>
2024-02-02 14:59:29 +0100

    Generalize append_to

--- challenges/doubleLinkedList.c
+++ challenges/doubleLinkedList.c
@@ -15,7 +15,18 @@ void append_to(node* parent, content val) {
     node* child = (node*) malloc(sizeof(node));
     child->content = val;
     child->prev = parent;
-    parent->next = child;
+
+    if (parent->next == NULL){
+        parent->next = child;
+    //insert child behind parent append what was previously appended to parent
+    } else { 
+        node* parent_next = parent->next;
+        parent->next = child;
+        child->next = parent_next;
+        parent_next->prev = child;
+    }
+
+
 }
 
 int main() {
@@ -30,7 +41,13 @@ int main() {
     head->content = 2;
     assert(head->content == 2);
 
+    append_to(head, 4);
+    assert(head->next->content == 4);
+
     append_to(head, 3);
     assert(head->next->content == 3);
+    assert(head->next->next->content == 4);
+
+
     return 0;
 }
