7c62bb64c0a444784855b5e4b0f471d8d2866da6
David Moravec <moravec@numsolution.cz>
2024-12-02 15:05:29 +0100

    Add expr

---
+++ golox/internal/expr/expr.go
@@ -0,0 +1,60 @@
+package expr
+
+type Expr interface {
+	accept(ExprVisitor)
+}
+
+type Operator byte
+
+type UnaryExpr struct {
+	operator Operator
+	right    Expr
+}
+
+func (e *UnaryExpr) accept(visitor ExprVisitor) {
+	visitor.visitUnaryExpr(e)
+}
+
+type BinaryExpr struct {
+	left     Expr
+	right    Expr
+	operator Operator
+}
+
+func (e *BinaryExpr) accept(visitor ExprVisitor) {
+	visitor.visitBinaryExpr(e)
+}
+
+type LiteralType int
+
+const (
+	NumberType LiteralType = iota
+	StringType
+	BoolType
+	NilType
+)
+
+type LiteralExpr struct {
+	lit_type LiteralType
+	number   int
+	str      string
+}
+
+func (e LiteralExpr) accept(visitor ExprVisitor) {
+	visitor.visitLiteralExpr(&e)
+}
+
+type GroupingExpr struct {
+	expression Expr
+}
+
+func (e *GroupingExpr) accept(visitor ExprVisitor) {
+	visitor.visitGroupingExpr(e)
+}
+
+type ExprVisitor interface {
+	visitUnaryExpr(expr *UnaryExpr)
+	visitBinaryExpr(expr *BinaryExpr)
+	visitLiteralExpr(expr *LiteralExpr)
+	visitGroupingExpr(expr *GroupingExpr)
+}
---
+++ golox/internal/expr/printer.go
@@ -0,0 +1,67 @@
+package expr
+
+import (
+	"bytes"
+	"strconv"
+)
+
+type AstPrinter struct {
+	expr_formated string
+	ast_formated  string
+}
+
+func (p *AstPrinter) Print_ast(expr Expr) string {
+	p.ast_formated = ""
+
+	expr.accept(p)
+
+	return p.ast_formated
+}
+
+func (p *AstPrinter) format_expr(expr Expr) string {
+	p.expr_formated = ""
+
+	expr.accept(p)
+
+	return p.expr_formated
+}
+
+func (p *AstPrinter) visitUnaryExpr(expr *UnaryExpr) {
+	p.parenthesize(string(expr.operator), []Expr{expr.right})
+
+}
+func (p *AstPrinter) visitBinaryExpr(expr *BinaryExpr) {
+	p.parenthesize(string(expr.operator), []Expr{expr.left, expr.right})
+
+}
+func (p *AstPrinter) visitLiteralExpr(expr *LiteralExpr) {
+	switch expr.lit_type {
+	case NumberType:
+		p.expr_formated = strconv.Itoa(expr.number)
+	case StringType:
+		p.expr_formated = expr.str
+	case BoolType:
+		p.expr_formated = strconv.FormatBool(expr.number != 0)
+	case NilType:
+		p.expr_formated = "nil"
+	}
+
+}
+func (p *AstPrinter) visitGroupingExpr(expr *GroupingExpr) {
+	p.parenthesize("group", []Expr{expr.expression})
+}
+
+func (p *AstPrinter) parenthesize(name string, expressions []Expr) {
+	s := new(bytes.Buffer)
+
+	s.WriteByte('(')
+	s.WriteString(name)
+
+	for _, e := range expressions {
+		s.WriteString(" " + p.format_expr(e))
+
+	}
+	s.WriteByte(')')
+
+	p.ast_formated += s.String()
+}
